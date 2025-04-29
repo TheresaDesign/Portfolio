9261ac6e585f57e590a4084b8d8c6ec4d9ed29e8
TheresaDesign <resab@t-online.de>
2025-04-29 19:43:42 +0200

    header etwas responsive

--- script.js
+++ script.js
@@ -18,7 +18,18 @@
    }
  });
 
-
+//fächer
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible');
+      }
+    });
+  }, { threshold: 0.5 });
+  
+  observer.observe(document.getElementById('imageContainer'));
   
   
  // Floating Words Animation
